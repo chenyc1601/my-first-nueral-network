6fc1522d821a5c096f81391d50a73f9888366e92
cyc <chenyc1601@gmail.com>
2018-12-03 15:01:00 +0800

    test imgs

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 
 from dataset import Dataset
 from neural_network import NeuralNetwork
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import pickle as pk
+import argparse
 
 if __name__ == "__main__" :
     # 数据集路径
     trainFilePath = "mnist_dataset/mnist_train.csv"
     testFilePath = "mnist_dataset/mnist_test.csv"
+    trainedNetworkPath = ""
+
+    """ # 定义参数
+    parser = argparse.ArgumentParser()
+    parser.add_argument("imgFile", type=str, help="待识别的图片文件")
+    parser.add_argument("--demo", )
+    args = parser.parse_args() """
 
     # 输入层，中间层和输出层的节点数
     inputNodes = 28 * 28  # 28*28像素  
@@ -17,19 +26,20 @@ if __name__ == "__main__" :
     # 学习率
     learningRate = 0.1
 
-    # 读取和预处理数据集
-    trainSet = Dataset(trainFilePath)
-    testSet = Dataset(testFilePath)
-    print("finish reading data")
+    # 读取或训练网络
+    if trainedNetworkPath :  # 读取
+        pass
+    else :  # 训练
+        n = NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
+        print("网络初始化完成")
 
-    # 建立并训练网络
-    n = NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
-    print("finish initiating nn")
-    for data in trainSet.dataList :
-        n.train(data['input'], data['target'])
-    print("finish training")
+        trainSet = Dataset(trainFilePath)
+        for data in trainSet.dataList :
+            n.train(data['input'], data['target'])
+        print("训练完成")
 
     # 测试
+    testSet = Dataset(testFilePath)
     errorCount = 0
     totalCount = 0
     for data in testSet.dataList :
@@ -39,8 +49,10 @@ if __name__ == "__main__" :
             ## print(result)  # 显示结果
             errorCount += 1  # 记录错误
             ## imageArray = data['input'].reshape((28, 28))  # 显示错误图像
-            ## matplotlib.pyplot.imshow(imageArray, cmap='Greys', interpolation='None').make_image()
+            ## plt.imshow(imageArray, cmap='Greys', interpolation='None')
+            ## plt.draw()
+            ## plt.pause(5)
     print(float(1 - errorCount / totalCount))
 
-    # test n.query()
-    ## print(n.query([1.0, 0.5, -1.5]))
+    # 实际识别
+    
