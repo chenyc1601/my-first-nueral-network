afe4fda8f825f72aacffc5225b34a1cc3b8f2953
cyc <chenyc1601@gmail.com>
2018-12-10 11:16:49 +0800

    周五结束状态

--- main.py
+++ main.py
@@ -14,35 +14,32 @@ if __name__ == "__main__" :
     parser.add_argument("-i", "--imgFile", help="待识别的图片文件", default="test_img/0.jpg")
     parser.add_argument("-t", "--train", help="重新训练网络", action="store_true")
     parser.add_argument("-e", "--echo", type=int, help="训练次数/世代", default=5)
-    parser.add_argument("-n", "--node", type=int, help="中间层结点数", default=200)
     parser.add_argument("-r", "--rate", type=float, help="学习率", default=0.1)
     args = parser.parse_args()
 
     # 数据集路径
     trainFilePath = "mnist_dataset/mnist_train.csv"  # 训练集
     testFilePath = "mnist_dataset/mnist_test.csv"  # 测试集
-    weightInHid = "w_I_H.pickle"
-    weightHidOut = "w_H_O.pickle"
-
-    # 网络输入层、中间层和输出层的节点数
-    inputNodes = 28 * 28  # 28*28像素  
-    hiddenNodes = args.node
-    outputNodes = 10  # 0-9十个数字
+    weight1 = "w1.pickle"
+    weight2 = "w2.pickle"
+    weight3 = "w3.pickle"
 
     # 学习率
     learningRate = args.rate
 
     # 初始化
     print("网络初始化开始...")
-    n = NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
+    n = NeuralNetwork(learningRate)
     print("网络初始化完成！")
 
     # 读取或训练网络
     if args.train != True :  # 读取
-        with open(weightInHid, 'rb') as handle :
-            n.w_I_H_n = pk.load(handle)
-        with open(weightHidOut, 'rb') as handle :
-            n.w_H_O_n = pk.load(handle)
+        with open(weight1, 'rb') as handle :
+            n.w1 = pk.load(handle)
+        with open(weight2, 'rb') as handle :
+            n.w2 = pk.load(handle)
+        with open(weight3, 'rb') as handle :
+            n.w3 = pk.load(handle)
     else :  # 训练
         print("读取训练集...")
         trainSet = Dataset(trainFilePath)
@@ -53,10 +50,12 @@ if __name__ == "__main__" :
                 n.train(data['input'], data['target'])
             print("第{0}次训练完成！".format(e + 1))
         # 保存训练结果
-        with open('w_I_H.pickle', 'wb') as handle :
-            pk.dump(n.w_I_H_n, handle, protocol=pk.HIGHEST_PROTOCOL)
-        with open('w_H_O.pickle', 'wb') as handle :
-            pk.dump(n.w_H_O_n, handle, protocol=pk.HIGHEST_PROTOCOL)        
+        with open('w1.pickle', 'wb') as handle :
+            pk.dump(n.w1, handle, protocol=pk.HIGHEST_PROTOCOL)
+        with open('w2.pickle', 'wb') as handle :
+            pk.dump(n.w2, handle, protocol=pk.HIGHEST_PROTOCOL)        
+        with open('w3.pickle', 'wb') as handle :
+            pk.dump(n.w3, handle, protocol=pk.HIGHEST_PROTOCOL)        
 
     # 测试
     testSet = Dataset(testFilePath)
--- neural_network.py
+++ neural_network.py
@@ -6,13 +6,14 @@ from scipy import special
 # neural network class definition
 class NeuralNetwork :
 
-    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate) :
+    def __init__(self, learningrate) :
         """根据输入的参数初始化神经网络
         """
         # 各层结点数
-        self.inodes = inputnodes
-        self.hnodes = hiddennodes
-        self.onodes = outputnodes
+        self.inodes = 28 * 28  # 28*28像素
+        self.h1nodes = 250  # 第一层
+        self.h2nodes = 50  # 第二层
+        self.onodes = 10  # 0-9十个数字
 
         # 学习率
         self.lr = learningrate
@@ -25,8 +26,9 @@ class NeuralNetwork :
         ## self.w_I_H_r = np.random.rand(self.hnodes, self.inodes) - 0.5  
         ## self.w_H_O_r = np.random.rand(self.onodes, self.hnodes) - 0.5
         # weights 正态分布，均值为0，标准差为1/传入链接数目的开方
-        self.w_I_H_n = np.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
-        self.w_H_O_n = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+        self.w1 = np.random.normal(0.0, pow(self.inodes, -0.5), (self.h1nodes, self.inodes))
+        self.w2 = np.random.normal(0.0, pow(self.h1nodes, -0.5), (self.h2nodes, self.h1nodes))
+        self.w3 = np.random.normal(0.0, pow(self.h2nodes, -0.5), (self.onodes, self.h2nodes))
 
         # 使用sigmoid函数作为结点的激发函数
         self.activation_function = lambda x: special.expit(x)
@@ -40,10 +42,13 @@ class NeuralNetwork :
         # convert inputs list to 2d array
         inputs = np.array(aInput, ndmin=2).T
 
-        hiddenInputs = np.dot(self.w_I_H_n, inputs)
-        hiddenOutputs = self.activation_function(hiddenInputs)
+        h1Inputs = np.dot(self.w1, inputs)
+        h1Outputs = self.activation_function(h1Inputs)
 
-        finalInputs = np.dot(self.w_H_O_n, hiddenOutputs)
+        h2Inputs = np.dot(self.w2, h1Outputs)
+        h2Outputs = self.activation_function(h2Inputs)
+
+        finalInputs = np.dot(self.w3, h2Outputs)
         finalOutputs = self.activation_function(finalInputs)
 
         return finalOutputs
@@ -55,20 +60,25 @@ class NeuralNetwork :
         # convert inputs list to 2d array
         inputs = np.array(aInput, ndmin=2).T
 
-        hiddenInputs = np.dot(self.w_I_H_n, inputs)
-        hiddenOutputs = self.activation_function(hiddenInputs)
+        h1Inputs = np.dot(self.w1, inputs)
+        h1Outputs = self.activation_function(h1Inputs)
 
-        finalInputs = np.dot(self.w_H_O_n, hiddenOutputs)
+        h2Inputs = np.dot(self.w2, h1Outputs)
+        h2Outputs = self.activation_function(h2Inputs)
+
+        finalInputs = np.dot(self.w3, h2Outputs)
         finalOutputs = self.activation_function(finalInputs)
 
         # 计算误差
         targets = np.array(aTarget, ndmin=2).T
         outputErrors = targets - finalOutputs
-        hiddenErrors = np.dot(self.w_H_O_n.T, outputErrors)
+        h2Errors = np.dot(self.w3.T, outputErrors)
+        h1Errors = np.dot(self.w2.T, h2Errors)
 
         # 调整权重
-        self.w_H_O_n += self.lr * np.dot(outputErrors * finalOutputs * (1.0 - finalOutputs), hiddenOutputs.T)
-        self.w_I_H_n += self.lr * np.dot(hiddenErrors * hiddenOutputs * (1.0 - hiddenOutputs), inputs.T)
+        self.w3 += self.lr * np.dot(outputErrors * finalOutputs * (1.0 - finalOutputs), h2Outputs.T)
+        self.w2 += self.lr * np.dot(h2Errors * h2Outputs * (1.0 - h2Outputs), h1Outputs.T)
+        self.w1 += self.lr * np.dot(h1Errors * h1Outputs * (1.0 - h1Outputs), inputs.T)
 
         return
 
