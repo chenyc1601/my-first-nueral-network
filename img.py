e474818cc4b54a2aac202e5d83f392b2896339f7
cyc <chenyc1601@gmail.com>
2018-12-12 14:40:25 +0800

    resize image to 28*28

--- img.py
+++ img.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import matplotlib.image as mpimg
 import PIL.Image as pImage
 import numpy as np
 
 class Image :
-    """将28*28 jpg RGB图片处理为用于识别的数据
+    """将jpg RGB图片处理为用于识别的数据
     """
     def __init__(self, imgPath) :
-        img = pImage.open(imgPath).convert('L')  # 读灰度图
-        array = np.array(img)  # 转矩阵
+        img = pImage.open(imgPath)
+        imgGray = img.convert('L')  # 读灰度图
+        img28 = imgGray.resize((28, 28))  # 转28*28
+        array = np.array(img28)  # 转矩阵
         array1_784 = array.reshape((1, 784))  # 转为1*784
         arrayInvert = 255.0 - array1_784  # 白底转黑底
         self.imgData = np.asfarray(arrayInvert) / 255.0 * 0.99 + 0.01
